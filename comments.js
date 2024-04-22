8f4595634eaa56e78ca058ac54fe3c558d50e56f
Ali-Mardan <62463181+Ali-Mardan@users.noreply.github.com>
2024-04-22 19:43:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,23 @@
-// create web server and listen on port 3000
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+// create web server
+// create a web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+
+// create a server
+http.createServer(function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var filepath = path.join(__dirname, 'public', pathname);
+    fs.stat(filepath, function(err, stats) {
+        if (err) {
+            console.log(err);
+            response.writeHead(404, {'Content-Type': 'text/html'});
+            response.end('<h1>404 Not Found</h1>');
+        } else {
+            fs.createReadStream(filepath).pipe(response);
+        }
+    });
+}).listen(3000);
+
+console.log('Server running at http://)
